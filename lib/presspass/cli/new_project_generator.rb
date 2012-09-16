fd9efc90f3e6ad0ee95bdc0981418237df2c74d6
Michiel Sikkes <michiel@firmhouse.com>
2012-09-16 21:39:22 +0200

    Remove rewrite rule from config.ru to make it work with WordPress

--- lib/presspass/cli/new_project_generator.rb
+++ lib/presspass/cli/new_project_generator.rb
@@ -82,19 +82,7 @@ INDEXES = ['index.html','index.php', 'index.cgi']
 
 ENV['SERVER_PROTOCOL'] = "HTTP/1.1"
 
-use Rack::Rewrite do
-  rewrite %r{(.*/$)}, lambda {|match, rack_env|
-    INDEXES.each do |index|
-      if File.exists?(File.join(Dir.getwd, rack_env['PATH_INFO'], index))
-        return rack_env['PATH_INFO'] + index
-      end
-    end
-    rack_env['PATH_INFO']
-  }
-end
-
 use Rack::Legacy::Php, Dir.getwd<% if options[:php_cgi_path] %>, '<%= options[:php_cgi_path] %>' <% end %>
-use Rack::Legacy::Cgi, Dir.getwd
 run Rack::File.new Dir.getwd
 EOF
         config_ru_template = ERB.new(config_ru)
